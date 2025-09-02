719154716297285ca0b156cb97996a74d4ee5573
seong-corder <tjdguswp333@gmail.com>
2025-09-02 12:52:08 +0900

    Update announcement.sql

--- announcement.sql
+++ announcement.sql
@@ -12,10 +12,14 @@ SELECT *
 FROM announcement
 WHERE announcement_code = 3;
 
+ -- 프로시져 삭제
+DROP PROCEDURE sp_add_announcment;
+SHOW PROCEDURE STATUS WHERE Db = 'fairclass';
+
  -- 공지사항 작성
 DELIMITER //
 CREATE PROCEDURE sp_add_announcement (
-    IN p_admin_code BIGINT,
+    IN p_user_code BIGINT,
     IN p_title VARCHAR(255),
     IN p_content TEXT,
     IN p_image VARCHAR(255),
@@ -23,25 +27,47 @@ CREATE PROCEDURE sp_add_announcement (
 )
 BEGIN
     -- 관리자 권한 체크
-    IF EXISTS (SELECT 1 FROM administrator WHERE admin_code = p_admin_code) THEN
+    IF EXISTS (SELECT 1 FROM administrator WHERE user_code = p_user_code) THEN
         INSERT INTO announcement (admin_code, title, content, image, posted_date, public)
-        VALUES (p_admin_code, p_title, p_content, p_image, NOW(), p_public);
+        VALUES (p_user_code, p_title, p_content, p_image, NOW(), p_public);
     ELSE
         SIGNAL SQLSTATE '45000'
             SET MESSAGE_TEXT = '권한이 없습니다: 관리자만 공지사항을 작성할 수 있습니다.';
     END IF;
 END //
 DELIMITER ;
+ -- 테스트 케이스(공지사항 작성)
+CALL sp_add_announcement(
+    1,                          -- p_user_code (여기서는 admin_code와 같음)
+    '테스트 공지',               -- p_title
+    '테스트 내용입니다',         -- p_content
+    'test.png',                 -- p_image
+    'Y'                         -- p_public
+);
+
+SELECT * FROM announcement ORDER BY posted_date DESC;
+
+ -- 관리자 아닌 계정
+CALL sp_add_announcement(
+    2,                        -- 존재하지 않는 admin_code
+    '잘못된 공지',               -- p_title
+    '학생이 작성하려는 내용',    -- p_content
+    NULL,                       -- p_image
+    'Y'                         -- p_public
+);
+
+
+
 
  -- 공지사항 삭제(soft delete)
 DELIMITER //
 CREATE PROCEDURE sp_delete_announcement (
-    IN p_admin_code BIGINT,
+    IN p_user_code BIGINT,
     IN p_announcement_code BIGINT
 )
 BEGIN
     -- 관리자 권한 체크
-    IF EXISTS (SELECT 1 FROM administrator WHERE admin_code = p_admin_code) THEN
+    IF EXISTS (SELECT 1 FROM administrator WHERE user_code = p_user_code) THEN
         UPDATE announcement
         SET public = 'N'
         WHERE announcement_code = p_announcement_code;
@@ -51,11 +77,28 @@ BEGIN
     END IF;
 END //
 DELIMITER ;
+ 
+ -- 테스트케이스(공지사항 삭제)
+CALL sp_delete_announcement(1, 3);
 
+SELECT announcement_code, title, public 
+FROM announcement
+WHERE announcement_code = 3;
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
  -- 공지사항 수정
 DELIMITER //
 CREATE PROCEDURE sp_update_announcement (
-    IN p_admin_code BIGINT,
+    IN p_user_code BIGINT,
     IN p_announcement_code BIGINT,
     IN p_title VARCHAR(255),
     IN p_content TEXT,
@@ -63,7 +106,7 @@ CREATE PROCEDURE sp_update_announcement (
 )
 BEGIN
     -- 관리자 권한 체크
-    IF EXISTS (SELECT 1 FROM administrator WHERE admin_code = p_admin_code) THEN
+    IF EXISTS (SELECT 1 FROM administrator WHERE user_code = p_user_code) THEN
         UPDATE announcement
         SET title = p_title,
             content = p_content,
@@ -76,6 +119,22 @@ BEGIN
     END IF;
 END //
 DELIMITER ;
+
+ -- 테스트크케이스(수정)
+CALL sp_update_announcement(
+    1,                      -- 관리자 user_code
+    3,                      -- 수정할 공지번호
+    '수정된 제목',           -- 새 제목
+    '수정된 내용',           -- 새 내용
+    'update.png'            -- 새 이미지
+);
+
+
+SELECT announcement_code, title, content, image, posted_date
+FROM announcement
+WHERE announcement_code = 3;
+
+
 
  -- 공지사항 목록 조회(프로시저)
 DROP PROCEDURE IF EXISTS sp_get_announcements;
